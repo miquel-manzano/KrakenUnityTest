0487e7cca8bd68f9a399843101af5f6c56f897ad
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-10-07 16:40:06 +0200

    Add player.cs

---
+++ KrakenUnityTest/Assets/Assets/player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/*
+Lorem ipsum dolor sit amet consectetur adipiscing elit sem natoque, 
+dis primis orci non turpis himenaeos at magnis a aenean, ante augue tristique hac eu risus porttitor sodales. 
+Venenatis duis ligula faucibus nulla purus diam, in curabitur tempor rhoncus id, ornare vehicula dictumst arcu euismod. 
+Duis ut pulvinar massa laoreet montes orci donec penatibus, mauris cras rutrum torquent fringilla fames mattis.
+ */
